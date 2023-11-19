02ffd5dd004801cb85eb562d361c7b95c25a7f37
GanlingZ <gz2184@nyu.edu>
2023-11-19 01:42:16 -0500

    add a third led and make it blink

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,8 +1,12 @@
 int ledPin = 13;
 int ledPin2 = 14;
+
+int ledPin3 = 15;
+
 void setup() {
   pinMode(ledPin,OUTPUT);
   pinMode(ledPin2,OUTPUT);
+  pinMode(ledPin3, OUTPUT);
 
 }
 
@@ -13,4 +17,8 @@ void loop() {
   digitalWrite(ledPin, LOW);
   digitalWrite(ledPin2, LOW);
   delay(500);
+  digitalWrite(ledPin3, HIGH);
+  delay(300);
+  digitalWrite(ledPin3,LOW);
+  delay(300);
 }
